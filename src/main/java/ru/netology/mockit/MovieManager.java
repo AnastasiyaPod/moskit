1a03ce55e5cb53ced196f30365f4eec9ff4e8479
AnastasiyaPod <an_pod12@mail.ru>
2023-12-11 03:12:29 +1000

    Initial commit

---
+++ src/main/java/ru/netology/mockit/MovieManager.java
@@ -0,0 +1,46 @@
+package ru.netology.mockit;
+public class MovieManager {
+    private String[] movies = new String[0];
+    private final int limit;
+    public MovieManager() {
+        this.limit = 5;
+    }
+    public MovieManager(int limit) {
+        this.limit = limit;
+    }
+    public void addMovie(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        tmp[tmp.length - 1] = movie;
+        this.movies = tmp;
+    }
+    public String[] findAll() {
+        return movies;
+    }
+    public String[] findLast() {
+        int resultLength;
+        if (movies.length < limit) {
+            resultLength = movies.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] ans = new String[resultLength];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = movies[movies.length - 1 - i];
+        }
+        return ans;
+    }
+    public String[] MaxFindLast() {
+        int resultLength;
+        if (movies.length > limit) {
+            resultLength = movies.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] ans = new String[resultLength];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = movies[movies.length - 1 - i];
+        }
+        return ans;
+    }
+}
---
+++ src/test/java/ru/netology/mockit/MovieManagerTest.java
@@ -0,0 +1,91 @@
+package ru.netology.mockit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class MovieManagerTest {
+    @Test
+    public void noMovieTest() {
+        MovieManager manager = new MovieManager();
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void addMovieTest() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Бладшот");
+        String[] expected = {"Бладшот"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void addMoviesTest() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперед");
+        manager.addMovie("Джентльмены");
+        String[] expected = {"Бладшот", "Вперед", "Джентльмены"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void maxAddMoviesTest() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперед");
+        manager.addMovie("Джентльмены");
+        manager.addMovie("Человек-неведимка");
+        manager.addMovie("Тролли");
+        String[] expected = {"Бладшот", "Вперед", "Джентльмены", "Человек-неведимка", "Тролли"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void moreMaxAddMovieTest() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперед");
+        manager.addMovie("Джентльмены");
+        manager.addMovie("Человек-неведимка");
+        manager.addMovie("Тролли");
+        manager.addMovie("Номер один");
+        String[] expected = {"Бладшот", "Вперед", "Джентльмены", "Человек-неведимка", "Тролли", "Номер один"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void findLastMovieTest() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперед");
+        manager.addMovie("Джентльмены");
+        String[] expected = {"Джентльмены", "Вперед", "Бладшот"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void maxFindLastMovieTest() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперед");
+        manager.addMovie("Джентльмены");
+        manager.addMovie("Человек-неведимка");
+        manager.addMovie("Тролли");
+        String[] expected = {"Тролли", "Человек-неведимка", "Джентльмены", "Вперед", "Бладшот"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void MoreFindLastMovieTest() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперед");
+        manager.addMovie("Джентльмены");
+        manager.addMovie("Человек-неведимка");
+        manager.addMovie("Тролли");
+        manager.addMovie("Номер один");
+        manager.addMovie("Отель");
+        String[] expected = {"Отель", "Номер один", "Тролли", "Человек-неведимка", "Джентльмены", "Вперед", "Бладшот"};
+        String[] actual = manager.MaxFindLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
